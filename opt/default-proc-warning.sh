b0a3d54ec66fe9d4e9eca9ccfe49c56b0d924092
François KUBLER <francois+github@kubler.org>
2025-02-26 11:13:35 +0100

    fix: update to fit Scalingo's env

--- opt/default-proc-warning.sh
+++ opt/default-proc-warning.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-if [ ! -f /app/Procfile ] && [ "${DYNO}" = "web.1" ]; then
-	echo "Create a Procfile to customize the command used to run this process: https://devcenter.heroku.com/articles/procfile"
+if [ ! -f /app/Procfile ] && [ "${CONTAINER}" = "web-1" ]; then
+	echo "Create a Procfile to customize the command used to run this process: https://doc.scalingo.com/platform/app/procfile"
 fi
