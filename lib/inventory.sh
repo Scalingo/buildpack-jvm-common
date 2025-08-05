1810ed9785721279ac439d51ea74be623cee0604
François KUBLER <francois+github@kubler.org>
2025-08-05 15:31:14 +0200

    chore(scalingo-20): keep scalingo-20 files in their own inventory

--- lib/inventory.sh
+++ lib/inventory.sh
@@ -13,11 +13,18 @@ JVM_COMMON_DIR="${JVM_COMMON_DIR:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && cd ..
 inventory::query() {
 	local raw_version_string="${1}"
 	local stack="${2}"
+	local inventory_file="inventory.json"
 
 	# Inventory file is done for heroku,so we have to update $stack:
 	stack="${stack/scalingo/heroku}"
 	# Remove '-minimal' from $stack:
 	stack="${stack/-minimal/}"
+
+	# We keep a specific inventory file for scalingo-20
+	# The regular inventory file is updated, the -20 one is not.
+	if [ "${stack}" = "heroku-20" ]; then
+		inventory_file="inventory-20.json"
+	fi
 
 	local default_distribution="zulu"
 
@@ -28,7 +35,7 @@ inventory::query() {
 		.artifacts[] | select(.version == $version and .metadata.distribution == $distribution and .arch == "amd64" and .os == "linux" and ((.metadata.cedar_stacks // []) | index($stack) != null))
 	INVENTORY_QUERY
 
-	jq <"${JVM_COMMON_DIR}/inventory.json" \
+	jq <"${JVM_COMMON_DIR}/${inventory_file}" \
 		--exit-status \
 		--compact-output \
 		--arg raw_version_string "${raw_version_string}" \
